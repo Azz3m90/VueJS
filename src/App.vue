90290201f198c56aab574d5c47fc9e83483f248c
Eng. Azzam Aziz Ali <Azzamazezali@gmail.com>
2024-04-19 07:34:16 +0300

    binding classes

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,46 @@
 
 <template>
   <!--Text interpolation -->
- <div>Hello {{ name }}  </div> 
- <div v-text="channel"></div>
- <h3 v-text="facebook"></h3>
- <p v-html="channel_html"></p>
- <p v-html="hack"></p>
+ <div>Hello {{ name }}  </div> <br>
+ <div v-text="channel"></div><br>
+ <h3 v-text="facebook"></h3><br>
+ <p v-html="channel_html"></p><br>
+ <p v-html="hack"></p><br>
+ <h1 v-bind:id='headingId'>Heading</h1><br>
+ <button v-bind:disabled="isDisabled">click me disabled</button><br>
+ <button v-bind:disabled="isNotDisabled">click me not disabled</button><br>
+ <h1 class="underline">underlined text </h1><br>
+ <h1 v-bind:class="success">danger </h1><br>
+ <h1 v-bind:class="isPromoted && 'promoted'">Pomoted movie </h1><br>
+ <h1 v-bind:class="isSoldOut ? 'soldout' : 'new'">soldout? movie </h1><br>
+ <h1 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'soldout' : 'new']">Array conditional movie  </h1><br>
+ <h1 v-bind:class="{
+  promoted:isPromoted,
+  new:!isSoldOut,
+  soldout:isSoldOut
+ }">Object conditional movie  </h1><br>
  
 </template>
 
 <style scoped>
-
+  .underline{
+    text-decoration: underline;
+  }
+  .danger{
+    background-color: red;
+  }
+    .success{
+    background-color: green;
+  }
+  .promoted{
+    font-style: italic;
+  }
+  .new{
+    color: olive;
+  }
+  .soldout{
+    color: red;
+  }
 </style>
 <script>
 export default {
@@ -22,7 +52,14 @@ export default {
       channel:'Eng.Azzam Aziz Ali',
       facebook:'Azzam Aziz Ali',
       channel_html:"<h3><i>channel</i></h3>",
-      hack:`<a href='#' onclick='alert("You have been hacked.")'>Win a prize</a>`
+      hack:`<a href='#' onclick='alert("You have been hacked.")'>Win a prize</a>`,
+      headingId:"Heading",
+      isDisabled:true,
+      isNotDisabled:false,
+      danger:"danger",
+      success:"success",
+      isPromoted:true,
+      isSoldOut:true,
     };
   }
 }
