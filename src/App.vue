f3feb2790fad960b8acf59aad9c659bcf511739b
Eng. Azzam Aziz Ali <Azzamazezali@gmail.com>
2024-04-24 08:16:50 +0300

    computed v-for rendering

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@
 <button @click="items.push({id:4,title:'mouse',price:75})">Add items</button>
 <h2>computed total : {{ total }}</h2>
 <h2>method total : {{ getTotal() }}</h2>
-<input type="text" v-model="country"/>
+<input type="text" v-model="country"/><br>
+<template v-for="item in items":key="item.id">
+  <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
+</template>
+  <h2 v-for="item in expensiveItems">{{ item.title }} {{ item.price }}</h2>
 </template>
 
 <script>
@@ -50,6 +54,9 @@ export default {
     total(){
        console.log('computed method is called')
       return this.items.reduce((total,curr) => (total = total + curr.price),0)
+    },
+    expensiveItems(){
+      return this.items.filter(item =>item.price > 100)
     }
   },
 }
