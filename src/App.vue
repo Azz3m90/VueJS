80e9a9a7866ccb6286116cc0cd27e3b2f6e1cffc
Eng. Azzam Aziz Ali <Azzamazezali@gmail.com>
2024-04-24 23:09:08 +0300

    components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 
 
 <template>
-<h2>Volume Tracker(0-10)</h2>
-<h2>Current Volume - {{ volume }}</h2>
-<input type="text" v-model="movie">
-<input type="text" v-model="movieInfo.title">
-<input type="text" v-model="movieInfo.actor">
-<button @click="volume += 2">Increase</button>
-<button @click="volume -= 2">Decrease</button>
-<div>
-  <button @click="movieList = movieList.concat('new pushed movie')">Add Movie</button>
-</div>
+<Greetings />
 </template>
 
 <script>
+import Greetings from './components/Greetings.vue';
 export default {
   name:"App",
+  components:{
+    Greetings,
+  },
   data(){
     return {
-      volume : 0 ,
-      movie:'Batman',
-      movieInfo:{
-        title:'',
-        actor:''
-      },
-      movieList:['AzzamMovie', 'Allam Movie']
+    
     }
   },
   methods:{
@@ -34,30 +23,7 @@ export default {
     
 },
 watch:{
-  volume(newValue, oldValue){
-    if(newValue > oldValue &&newValue === 16){
-      alert('hearing high level volume for a long time may damage your hearing')
-    }
-  },
-  movie:{
-    handler(newValue){
-    console.log(`API has been called to fetch the movie name : ${newValue}`)},
-    immediate:true,
-   },
-   movieInfo:
-   {
-    handler(newValue){
-     console.log(`API has been called to fetch the movie name : ${newValue.title} and actor: ${newValue.actor}`)
-    },
-    deep:true
-  },
-  movieList:
-   {
-    handler(newValue){
-     console.log(`movieList Upldated : ${newValue} `)
-    },
-    //deep:true
-  },
+
 }
 }
 </script>
---
+++ src/components/Greetings.vue
@@ -0,0 +1,8 @@
+<template>
+ <h2>Hello Azzam</h2>
+</template>
+<script>
+export default {
+ name:'Greetings',
+}
+</script>
